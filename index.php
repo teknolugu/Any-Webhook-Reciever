57a73377c56dd356192c9af886ef267f9ec94863
Azhe Kun <azhe403@gmail.com>
2019-07-03 23:45:16 +0700

    fix

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use src\AzheSpace\Bot\Telegram\TelegramBot;
 include_once './src/autoload.php';
 include_once './Resources/Config/bot.php';
 
-if (strlen($_GET['chat_id']) > 9) {
+if (strlen($_GET['chat_id']) >= 9) {
 	$bot = new TelegramBot(BOT_TOKEN);
 	$bot->handle();
 } else {
