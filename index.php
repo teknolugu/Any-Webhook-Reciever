c8767d57749c35f11ee191c58799e8b8caf39329
Azhe Kun <azhe403@gmail.com>
2019-07-04 18:46:14 +0700

    add message

--- index.php
+++ index.php
@@ -8,6 +8,11 @@ include_once './Resources/Config/bot.php';
 if (strlen($_GET['chat_id']) >= 9) {
 	$bot = new TelegramBot(BOT_TOKEN);
 	$bot->handle();
+	echo "Sambungkan bot ke repository Anda." .
+		"<br>Silakan pasang url di bawah ini di pengaturan Webhook\Integrasi." .
+		"<br>https://integrate.azhe.space/236205726.php" .
+		"<br><br>Penyedia yang di dukung: GitLab, GitHub";
 } else {
 	print "you can try send /start in Bot";
+	header('Location: https://azhe.space');
 }
