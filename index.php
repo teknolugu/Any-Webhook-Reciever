c532e6896ec80fffb3ff1c8e8a5946a035b4b0f9
Azhe Kun <azhe403@gmail.com>
2019-07-04 18:28:35 +0700

    use main handler

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 
-use src\AzheSpace\Bot\Telegram\TelegramBot;
+use src\AzheSpace\Handler\Main;
 
 include_once './src/autoload.php';
 include_once './Resources/Config/bot.php';
 
 if (strlen($_GET['chat_id']) >= 9) {
-	$bot = new TelegramBot(BOT_TOKEN);
+    $bot = new Main();
 	$bot->handle();
 } else {
 	print "you can try send /start in Bot";
