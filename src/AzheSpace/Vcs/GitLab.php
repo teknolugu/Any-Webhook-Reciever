6ecc8aa8b80076a4aa410a9acdc16c6c6bb7a61f
Azhe Kun <azhe403@gmail.com>
2019-07-08 23:18:19 +0700

    GitLab issue event detection

--- src/AzheSpace/Vcs/GitLab.php
+++ src/AzheSpace/Vcs/GitLab.php
@@ -11,34 +11,51 @@ class GitLab
 	public function parseJson($json)
 	{
 		$datas = json_decode($json, true);
-		$event_name = $datas['event_name'];
-//        if ($event_name != '') {
-		$username = $datas['user_name'];
-		$commits = $datas['commits'];
-		$listCommit = "";
-		$no = 1;
-		foreach ($commits as $key => $val) {
-			$message = $val['message'];
-			$url = $val['url'];
-			$authorName = $val['author']['name'];
-			
-			$addedCount = count($val['added']);
-			$modifiedCount = count($val['modified']);
-			$removedCount = count($val['removed']);
-			
-			$listCommit .= "\n$no. <a href='$url'>$message</a>" . " By " . $authorName;
-			$no++;
-		}
+		
+		$event_name = $datas['event_type'];
+		$username = $datas['user']['name'];
+		
+		switch ($event_name) {
+			case 'push':
+				$commits = $datas['commits'];
+				$listCommit = "";
+				$no = 1;
+				foreach ($commits as $key => $val) {
+					$message = $val['message'];
+					$url = $val['url'];
+					$authorName = $val['author']['name'];
+					
+					$addedCount = count($val['added']);
+					$modifiedCount = count($val['modified']);
+					$removedCount = count($val['removed']);
+					
+					$listCommit .= "\n$no. <a href='$url'>$message</a> by <b>$authorName</b>" .
+						"\nAdded: $addedCount, Modified: $modifiedCount, Removed: $removedCount.\n";
+					$no++;
+				}
 
 //            $total_commit_count = $datas['total_commits_count'];
+				
+				$text = "<b>GitLab $event_name</b> by <b>$username</b>" . "$listCommit";
+				break;
+			case 'issue':
+				$atributes = $datas['object_attributes'];
+				$title = $atributes['title'];
+				$desc = $atributes['description'];
+				$urlIssue = $atributes['url'];
+				$dueDate = $atributes['due_date'] ?? '- no due date -';
+				$state = $atributes['state'];
+				
+				$text = "<b>GitLab Issue</b> $state by $username" .
+					"\nTitle: <a href='$urlIssue'>$title</a>" .
+					"\nDue date: $dueDate";
+				break;
+			
+			default:
+				$text = "GitLab Event not indentified, please ask @Azhe403";
+				break;
+		}
 		
-		$text = "<b>GitLab Event.</b>" .
-			"\n<b>$username $event_name</b>" .
-			"$listCommit";
-//			"\n\nAdded: $addedCount, Modified: $modifiedCount, Removed: $removedCount.";
-//        } else {
-//            $text = "Github";
-//        }
 		return $text;
 	}
 }
